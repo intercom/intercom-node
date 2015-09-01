50f9a11886187f0640c1eca97091a86cc7c4f7b0
Bob Long <robertjflong@gmail.com>
2015-09-01 14:12:35 +0100

    Conversations + messages

---
+++ test/conversation.js
@@ -0,0 +1,38 @@
+import assert from 'assert';
+import {Client} from '../lib';
+var nock = require('nock');
+
+describe('conversations', function () {
+  it('should be listed', function (done) {
+    nock('https://api.intercom.io').get('/conversations').query({ foo: 'bar' }).reply(200, {});
+    let client = new Client('foo', 'bar');
+    client.conversations.list({ foo: 'bar' }, function (r) {
+      assert.equal(200, r.status);
+      done();
+    });
+  });
+  it('should find', function (done) {
+    nock('https://api.intercom.io').get('/conversations/bar').query({ id: 'bar' }).reply(200, {});
+    let client = new Client('foo', 'bar');
+    client.conversations.find({ id: 'bar' }, function (r) {
+      assert.equal(200, r.status);
+      done();
+    });
+  });
+  it('should reply', function (done) {
+    nock('https://api.intercom.io').post('/conversations/bar/reply', { id: 'bar', baz: 'bang' }).reply(200, {});
+    let client = new Client('foo', 'bar');
+    client.conversations.reply({ id: 'bar', baz: 'bang' }, function (r) {
+      assert.equal(200, r.status);
+      done();
+    });
+  });
+  it('should mark as read', function (done) {
+    nock('https://api.intercom.io').put('/conversations/bar', { read: true }).reply(200, {});
+    let client = new Client('foo', 'bar');
+    client.conversations.markAsRead({ id: 'bar' }, function (r) {
+      assert.equal(200, r.status);
+      done();
+    });
+  });
+});
---
+++ test/message.js
@@ -0,0 +1,14 @@
+import assert from 'assert';
+import {Client} from '../lib';
+var nock = require('nock');
+
+describe('messages', function () {
+  it('should be created', function (done) {
+    nock('https://api.intercom.io').post('/messages', { message_type: 'foo' }).reply(200, {});
+    let client = new Client('foo', 'bar');
+    client.messages.create({ message_type: 'foo' }, function (r) {
+      assert.equal(200, r.status);
+      done();
+    });
+  });
+});
