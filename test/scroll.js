8c4269976f77b1dbb5c0dc1d407780eb347a6cd4
Louis-Rémi Babé <lrbabe@gmail.com>
2016-10-20 13:58:01 +0200

    make users.scroll return a promise and wait for inner promises

--- test/scroll.js
+++ test/scroll.js
@@ -1,10 +1,11 @@
+import Bluebird from 'bluebird';
 import assert from 'assert';
 import {Client} from '../lib';
 import nock from 'nock';
 
-describe('scroll', () => {
-  it('should get users with scroll', done => {
-    nock('https://api.intercom.io').get('/users/scroll').reply(200, {
+describe.only('scroll', () => {
+  before(function () {
+    nock('https://api.intercom.io').get('/users/scroll').times(3).reply(200, {
       type: 'user.list',
       scroll_param: '123_soleil',
       users: [
@@ -15,17 +16,59 @@ describe('scroll', () => {
         }
       ]
     });
-    nock('https://api.intercom.io').get('/users/scroll?scroll_param=123_soleil').reply(200, {
+
+    nock('https://api.intercom.io').get('/users/scroll?scroll_param=123_soleil').times(3).reply(200, {
       type: 'user.list',
       scroll_param: '123_soleil',
       users: []
     });
+  });
+
+  it('should get users with scroll', done => {
     const client = new Client('foo', 'bar');
+
     client.users.scroll.each({}, function (res) {
       assert.equal(200, res.status);
       if (res.body.users.length === 0) {
         done();
       }
+    });
+  });
+
+  it('should return a promise that resolves at the end of the scroll', done => {
+    const client = new Client('foo', 'bar');
+    let nbCalls = 0;
+
+    const promise = client.users.scroll.each({}, function () {
+      nbCalls++;
+    });
+
+    promise.then(() => {
+      assert.equal(2, nbCalls);
+
+      done();
+    });
+  });
+
+  it('should wait for promises returned by the callback before scrolling', done => {
+    const client = new Client('foo', 'bar');
+    let nbCalls = 0;
+
+    const promise = client.users.scroll.each({}, function (res) {
+      nbCalls++;
+
+      return res.body.users.length === 0 ?
+        null :
+        new Bluebird((resolve) => {
+          setTimeout(() => {
+            assert.equal(1, nbCalls, 'hasn\'t re-scrolled before resolve');
+            resolve();
+          }, 500);
+        });
+    });
+
+    promise.then(() => {
+      done();
     });
   });
 });
