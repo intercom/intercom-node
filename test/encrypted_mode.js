601bbc3c6f3a733b62043eeabba32de07e09063f
Bob Long <robertjflong@gmail.com>
2017-12-01 17:57:07 -0600

    spec name

--- test/encrypted_mode.js
+++ test/encrypted_mode.js
@@ -11,7 +11,7 @@ describe('encrypted mode', () => {
     done();
   });
 
-  it('calculates the payload correctly', done => {
+  it('calculates the payload correctly with a user_hash', done => {
     const expected = 'AAAAAAAAAAAAAAAA3dhRww8Q0+iDwrR/+EwhpcpZq7fKDE9oOsuwgEW7lH0zBMYKHVHr5r67877CANye3BCCyBoKVrOkxXR5htC7oUAaAfhCrfHUeWrjS+OW3GTI6b8/sZcrWJ0KlTyk0U07TXlOfALoXuOSruEiXrMtMkQzD8vO4J8=';
     const iv = new Buffer([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
     const payload = {email: 'test@foo.com'};
