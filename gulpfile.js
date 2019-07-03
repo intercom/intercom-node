bee71875300eac5954438db03fb131be18f9305d
Matt Donnelly <matthew.donnelly@intercom.io>
2019-07-03 17:49:02 +0100

    Update deps

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,9 @@ var istanbul = require('gulp-istanbul');
 var plumber = require('gulp-plumber');
 var babel = require('gulp-babel');
 
-// Initialize the babel transpiler so ES2015 files gets compiled
-// when they're loaded
-require('babel-core/register');
-
 gulp.task('static', function () {
-  return gulp.src('**/*.js')
+  return gulp
+    .src('**/*.js')
     .pipe(excludeGitignore())
     .pipe(eslint())
     .pipe(eslint.format())
@@ -19,31 +16,36 @@ gulp.task('static', function () {
 });
 
 gulp.task('pre-test', function () {
-  return gulp.src('lib/**/*.js')
+  return gulp
+    .src('lib/**/*.js')
     .pipe(babel())
-    .pipe(istanbul({includeUntested: true}))
+    .pipe(istanbul({ includeUntested: true }))
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr;
-
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
-    .on('error', function (err) {
-      mochaErr = err;
-    })
-    .on('end', function () {
-      cb(mochaErr);
-    });
-});
+gulp.task(
+  'test',
+  gulp.series('pre-test', function (done) {
+    var mochaErr;
+    gulp
+      .src('test/**/*.js')
+      .pipe(plumber())
+      .pipe(mocha({ reporter: 'spec' }))
+      .on('error', function (err) {
+        mochaErr = err;
+      })
+      .on('end', function () {
+        done(mochaErr);
+      });
+  })
+);
 
 gulp.task('babel', function () {
-  return gulp.src('lib/**/*.js')
+  return gulp
+    .src('lib/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('prepublish', ['babel']);
-gulp.task('default', ['static', 'test']);
+gulp.task('prepublish', gulp.series('babel'));
+gulp.task('default', gulp.series('static', 'test'));
