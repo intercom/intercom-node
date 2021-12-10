714f218d8df4aabfcc24198fdb834c236c0aef14
Volo <volo@Volos-MacBook-Pro.local>
2021-12-10 09:49:03 +0100

    Optimise nested objects type

--- lib/common/common.types.ts
+++ lib/common/common.types.ts
@@ -23,13 +23,15 @@ type Join<K, P> = K extends string | number ?
 type Prev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
     11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]]
 
-export type Paths<T, D extends number = 10> = [D] extends [never] ? never : T extends object ?
+// D is depth, we don't want to go past 3 nested objects
+export type Paths<T, D extends number = 3> = [D] extends [never] ? never : T extends object ?
     { [K in keyof T]-?: K extends string | number ?
         `${K}` | Join<K, Paths<T[K], Prev[D]>>
         : never
     }[keyof T] : ""
 
-export type Leaves<T, D extends number = 10> = [D] extends [never] ? never : T extends object ?
+// D is depth, we don't want to go past 3 nested objects
+export type Leaves<T, D extends number = 3> = [D] extends [never] ? never : T extends object ?
     { [K in keyof T]-?: Join<K, Leaves<T[K], Prev[D]>> }[keyof T] : "";
 
 
