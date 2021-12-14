c8945ede70b0173642736db512b2816e8997a2c8
Volo <volo@Volos-MacBook-Pro.local>
2021-12-14 10:09:52 +0100

    Add Messages endpoint

---
+++ lib/message.ts
@@ -0,0 +1,47 @@
+import { Client } from ".";
+import { MessageType } from "./message/message.types";
+
+export default class Message {
+  private messagesBaseUrl = 'messages'
+
+  constructor(private readonly client: Client) {
+    this.client = client;
+  }
+  
+  create({ messageType: message_type, subject, body, template, from, to }: ICreateMessageBody) {
+    const data: ICreateMessageRequest = {
+      message_type,
+      subject,
+      body,
+      template,
+      from,
+      to,
+    };
+
+    return this.client.post({url: `/${this.messagesBaseUrl}`, data});
+  }
+}
+
+interface ICreateMessageRequest {
+  message_type: MessageType,
+  body: string,
+  from: Recepient,
+  to: Recepient,
+  subject?: string
+  template?: string
+}
+
+interface ICreateMessageBody extends Omit<ICreateMessageRequest, 'message_type'> {
+  messageType: MessageType,
+}
+
+type Recepient = {
+  id: string,
+  type: RecepientType
+}
+
+export enum RecepientType {
+  ADMIN = 'admin',
+  USER = 'user',
+  LEAD = 'lead'
+}
--- lib/message/message.types.ts
+++ lib/message/message.types.ts
@@ -3,14 +3,14 @@ import { Timestamp } from "../common/common.types";
 export interface MessageObject {
   type: string,
   id: string,
-created_at: Timestamp,
-subject?: string,
-body: string,
-message_type: MessageType,
-conversation_id?: string,
+  created_at: Timestamp,
+  subject?: string,
+  body: string,
+  message_type: MessageType,
+  conversation_id?: string,
 }
 
-enum MessageType {
+export enum MessageType {
   EMAIL = 'email',
   INAPP = 'inapp',
   FACEBOOK = 'facebook',
