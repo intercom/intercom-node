a8f99e14104379e5c94acad88f5f4d21878b8cd6
Volo <volo@Volos-MacBook-Pro.local>
2022-02-11 17:57:29 +0200

    Fix pipeline
    Fix pipeline
    
    Fix pipeline
    
    Fix pipeline
    
    Fix pipeline
    
    Fix pipeline
    
    Fix pipeline
    
    Fix pipeline
    
    Fix pipeline
    
    Fix pipeline

--- test/integration/utils/config.ts
+++ test/integration/utils/config.ts
@@ -1,7 +1,1 @@
-const apiToken = process.env.API_TOKEN;
-
-if (!apiToken) {
-    throw new Error('API_TOKEN is required in env to run integration tests!');
-}
-
-export const token = apiToken;
+export const token = process.env.API_TOKEN;
